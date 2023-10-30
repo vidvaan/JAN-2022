f9073be48db10119757e8799546fc85df0e091cc
vidvaan <balajigvidvaan@gmail.com>
2023-10-30 20:32:49 +0530

    Add files via upload
    Test

--- 007-SpringSecurityDemo/src/main/java/com/ciq/SecurityConfig.java
+++ 007-SpringSecurityDemo/src/main/java/com/ciq/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,19 +19,28 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	
 	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
 	public UserDetailsService users() {
 		// The builder will ensure the passwords are encoded before saving in memory
-		UserBuilder users = User.withDefaultPasswordEncoder();
+	//	UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		UserBuilder users = User.builder();
 		UserDetails user = users
 			.username("balaji")
-			.password("balaji")
+			.password("$2a$10$sf8K/vCKM3hSvB0W8OzycOSZ1VD/UeHxI.JgniQLopXwYdH3uKM0u")
 			.roles("USER")
 			.build();
 		UserDetails admin = users
-			.username("admin")
-			.password("admin")
+			.username("srikanth")
+			.password("$2a$10$7CewQish1obRT2VwhEAL0.Tc.ujbSRd/J92Rx3IqX9.W401TJ7/B6")
 			.roles("ADMIN","USER")
 			.build();
+		
+		
 		return new InMemoryUserDetailsManager(user, admin);
 	}
 	
--- 007-SpringSecurityDemo/src/test/java/com/ciq/ApplicationTests.java
+++ 007-SpringSecurityDemo/src/test/java/com/ciq/ApplicationTests.java
@@ -2,12 +2,16 @@ package com.ciq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+		System.out.println(bc.encode("srikanth"));
 	}
 
 }
